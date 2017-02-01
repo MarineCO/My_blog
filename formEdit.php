ef0d40228a2f5383b02b230cccb7ad334e3a325b
marineco <marinecolonge@gmail.com>
2017-02-01 16:48:42 +0100

    création fichier contenant formulaire d'édition

---
+++ formEdit.php
@@ -0,0 +1,44 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+
+ORM::configure('mysql:host=localhost;dbname=my_blog');
+ORM::configure('username', 'root');
+ORM::configure('password', 'root');
+
+$id = $_POST['id'];
+
+$editArticle = ORM::for_table('posts')->find_one($id);
+
+$title = $editArticle->title;
+$content = $editArticle->content;
+$author = $editArticle->author;
+$id = $editArticle->id;
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Edition d'un nouvel article</title>
+</head>
+<body>
+
+	<form method="POST" action="submit_post.php">
+		
+		<input type="hidden" type="text" name="id" value="<?php $id ?>">
+
+		<label for="title">Titre</label>
+		<input id="title" type="text" name="title" value="<?= $title ?>">
+
+		<label for="content">Article</label>
+		<input id="content" type="text" name="content" value="<?= $content ?>">
+
+		<label for="author">Auteur</label>
+		<input id="author" type="text" name="author" value="<?= $author ?>">
+
+		<input value="Editer" type="submit" name="submit">
+
+	</form>
+
+</body>
+</html>
